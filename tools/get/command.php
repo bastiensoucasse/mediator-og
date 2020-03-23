a36888c3a6ab68515755647f26fc0713d3c7b65b
Bastien Soucasse <bastien-soucasse@PCBS21.home>
2020-03-23 21:42:04 +0100

    Correct command date display bug

--- tools/get/command.php
+++ tools/get/command.php
@@ -32,6 +32,6 @@ else
     <div class="command-id">#<?= $c["CommandID"] ?></div>
     <div class="command-info">
         <div class="command-title"><?= $c["Title"] ?></div>
-        <div class="command-desc"><?= $c["Type"] == "series" ? "Série" : "Film" . " • " . substr($c["Date"], 0, 10) ?></div>
+        <div class="command-desc"><?= ($c["Type"] == "series" ? "Série" : "Film") . " • " . substr($c["Date"], 0, 10) ?></div>
     </div>
 </div>
