82cb2da5a6e4e9487f6faecc5f967b246ba5900e
Bastien Soucasse <bastien.soucasse@laposte.net>
2020-03-29 21:07:06 +0200

    Correct notifications

--- tools/notif.php
+++ tools/notif.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_COOKIE["notification"]))
 {
+    setcookie("notification", "", time() - 3600, "/");
     $n = htmlspecialchars($_COOKIE["notification"]);
     echo("<div class=\"notification\">$n</div>");
-    setcookie("notification", "", time() - 3600, "/");
 }
