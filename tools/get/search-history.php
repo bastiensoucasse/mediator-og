6ed8ac6b6ed364f6c202c830f6fded41a391d96d
Bastien Soucass <bastien.soucasse@outlook.fr>
2020-04-05 20:33:59 +0200

    Correct bug

--- tools/get/search-history.php
+++ tools/get/search-history.php
@@ -3,7 +3,7 @@ require_once("../database.php");
 require_once("../init.php");
 
 $stmt = $db->prepare("SELECT `SearchID`, `Query` FROM `Searches` WHERE `User` = ? ORDER BY `Date` DESC LIMIT 8");
-$stmt->execute(array(htmlspecialchars($_SESSION["id"]));
+$stmt->execute(array(htmlspecialchars($_SESSION["id"])));
 $searches = $stmt->fetchAll();
 
 if (!$searches) echo("Vous n'avez effectué aucune recherche.");
