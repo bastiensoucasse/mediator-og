b95ed4188cc36f2bdeac2eeb9a4ee21d91e410b1
Bastien Soucasse <bastien.soucasse@outlook.fr>
2020-05-14 17:49:33 +0200

    Update head

--- include/head.php
+++ include/head.php
@@ -1,18 +1,14 @@
 <?php
-define("PROTOCOL", isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ? "https" : "http");
-define("HOST", $_SERVER["HTTP_HOST"]);
-define("BASE", PROTOCOL . "://" . HOST . "/");
 define("CANONICAL", "https://mediator.profuder.com/" . $page->id);
 if ($page->id == "home") define("TITLE", "Mediator");
 else define ("TITLE", $page->name . " - " . "Mediator");
 define("DESCRIPTION", $page->description);
 ?>
 <head>
-    <base href="<?= BASE ?>" />
     <link rel="canonical" href="<?= CANONICAL ?>" />
     <link rel="manifest" href="/manifest.webmanifest">
     <link rel="shortcut icon" href="/favicon.png" />
-    <link rel="apple-touch-icon" href="/favicon.png" />
+    <link rel="apple-touch-icon" href="/images/logo/mediator-192.png" />
     <meta charset="utf-8" />
     <meta name="description" content="<?= DESCRIPTION ?>" />
     <meta name="theme-color" content="#14AB95" />
