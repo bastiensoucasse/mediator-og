fa9bf578b01141d7a887ee7ab9585c0fa0bc7ff1
Bastien Soucasse <bastien-soucasse@outlook.fr>
2020-08-17 16:18:47 +0200

    Fix collections design

--- include/collection.php
+++ include/collection.php
@@ -1,3 +1,3 @@
 <a class="collection" href="<?= "/collections?id=$collection->id" ?>" aria-label="<?= $collection->name ?>">
-    <h4 class="collection-name"><?= $collection->name ?></h4>
+    <h6 class="collection-name"><?= $collection->name ?></h6>
 </a>
