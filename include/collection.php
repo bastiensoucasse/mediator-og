f74f185e61794c9cc9c889ba461f6cc89ef150e4
Bastien Soucasse <bastien-soucasse@outlook.fr>
2020-08-17 15:06:08 +0200

    Fix collection links

--- include/collection.php
+++ include/collection.php
@@ -1,3 +1,3 @@
-<a class="collection" href="<?= "/collection?id=$collection->id" ?>" aria-label="<?= $collection->name ?>">
+<a class="collection" href="<?= "/collections?id=$collection->id" ?>" aria-label="<?= $collection->name ?>">
     <h4 class="collection-name"><?= $collection->name ?></h4>
 </a>
